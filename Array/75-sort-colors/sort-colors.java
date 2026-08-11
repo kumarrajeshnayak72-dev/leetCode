a2e0b21a6d4f5cba571718334fc86b5008d4b612
Rajesh Kumar Nayak <kumarrajeshnayak72@gmail.com>
2026-08-11 08:00:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (24.93%) - LeetSync

---
+++ Array/75-sort-colors/sort-colors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == 0){
+                red++;
+            }else if(nums[i] == 1){
+                white++;
+            }else{
+                blue++;
+            }
+        }
+
+        int j = 0;
+        while(j < nums.length){
+            if(red > 0){
+                nums[j] = 0;
+                j++;
+                red--;
+            }else if(white > 0){
+                nums[j] = 1;
+                j++;
+                white--;
+            }else{
+                nums[j] = 2;
+                j++;
+                blue--;
+            }
+        }
+    }
+}
