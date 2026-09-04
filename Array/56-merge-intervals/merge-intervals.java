91c03e7ed9672eafb45aa21b016d88132f3e0758
Rajesh Kumar Nayak <kumarrajeshnayak72@gmail.com>
2026-09-04 17:02:02 +0530

    Time: 8 ms (91.10%) | Memory: 49.3 MB (26.87%) - LeetSync

--- Array/56-merge-intervals/merge-intervals.java
+++ Array/56-merge-intervals/merge-intervals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> ans = new ArrayList<>();
         int n = intervals.length;
@@ -8,17 +8,21 @@ class Solution {
         int start = intervals[0][0];
         int end = intervals[0][1];
 
-        for(int i = 1; i < n; i++){
-            if(end >= intervals[i][0]){
+        for (int i = 1; i < n; i++) {
+            if (end >= intervals[i][0]) {
+                // Intervals overlap, so extend the end
                 end = Math.max(end, intervals[i][1]);
-            }else{
+            } else {
+                // No overlap, save the current interval
                 ans.add(new int[]{start, end});
 
+                // Start a new interval
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
 
+        // Add the last interval
         ans.add(new int[]{start, end});
 
         return ans.toArray(new int[ans.size()][]);
