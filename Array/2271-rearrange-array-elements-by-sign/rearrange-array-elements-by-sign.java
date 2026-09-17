bb5b97fb9be080ac22b1d87f28a35e7f518b1a41
Rajesh Kumar Nayak <kumarrajeshnayak72@gmail.com>
2026-09-17 07:45:42 +0530

    Time: 3 ms (100.00%) | Memory: 175.4 MB (57.94%) - LeetSync

--- Array/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ Array/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int [] res = new int[nums.length];
+        int [] res = new int [nums.length];
+        int i = 0, j = 1;
 
-        int pos = 0;
-        int neg = 1;
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > 0){
-                res[pos] = nums[i];
-                pos += 2;
+        for(int k = 0; k < nums.length; k++){
+            if(nums[k] > 0){
+                res[i] = nums[k];
+                i += 2;
             }else{
-                res[neg] = nums[i];
-                neg += 2;
+                res[j] = nums[k];
+                j += 2;
             }
         }
         return res;
